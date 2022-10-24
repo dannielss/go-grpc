1111011cc3f93e58b6dc7d91d2628fb6d44cd2f5
Daniel <daniel_sousa.p@outlook.com>
2022-10-23 21:36:20 -0300

    feat: send a request to GetUsers RPC

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"io"
 	"log"
+	"time"
 
 	"github.com/dannielss/go-grpc/pb"
 	"google.golang.org/grpc"
@@ -22,11 +24,34 @@ func main() {
 		Message: "Hello GRPC",
 	}
 
-	res, err := client.RequestMessage(context.Background(), req)
+	// Unary RPC
+	// res, err := client.RequestMessage(context.Background(), req)
+
+	// Server streaming RPC
+	stream, err := client.GetUsers(context.Background(), req)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Print(res.GetStatus())
+	done := make(chan bool)
+
+	go func() {
+		for {
+			resp, err := stream.Recv()
+			if err == io.EOF {
+				done <- true
+				return
+			}
+			if err != nil {
+				log.Fatalf("can not receive %v", err)
+			}
+			log.Printf("Resp received: %+v", resp)
+			time.Sleep(2 * time.Second)
+
+		}
+	}()
+
+	<-done
+	log.Printf("finished")
 }
