1f32c95f6a275e9709d4b4e516a3a5946a8a74a6
Daniel <daniel_sousa.p@outlook.com>
2022-10-23 21:35:49 -0300

    feat: implement RPC

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"net"
+	"sync"
 
 	"github.com/dannielss/go-grpc/pb"
 	"google.golang.org/grpc"
@@ -23,6 +24,43 @@ func (s *Server) RequestMessage(ctx context.Context, req *pb.Request) (*pb.Respo
 	}
 
 	return response, nil
+}
+
+func (s *Server) GetUsers(req *pb.Request, u pb.SendMessage_GetUsersServer) error {
+	log.Print("message received: ", req.GetMessage())
+
+	users := []*pb.User{
+		{Name: "Daniel", Age: 21},
+		{Name: "Daniela", Age: 22},
+	}
+
+	// sequentially
+
+	// for _, user := range users {
+	// 	resp := pb.User{Name: user.Name, Age: user.Age}
+
+	// 	if err := u.Send(&resp); err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// }
+
+	// concurrently
+
+	var wg sync.WaitGroup
+	for _, user := range users {
+		wg.Add(1)
+		go func(user *pb.User) {
+			defer wg.Done()
+			resp := pb.User{Name: user.GetName(), Age: user.GetAge()}
+			if err := u.Send(&resp); err != nil {
+				log.Printf("send error %v", err)
+			}
+		}(user)
+	}
+
+	wg.Wait()
+
+	return nil
 }
 
 func (s *Server) mustEmbedUnimplementedSendMessageServer() {}
